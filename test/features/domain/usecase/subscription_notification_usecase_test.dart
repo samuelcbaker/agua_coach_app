4bbe5b487163bf64ea74a50825364a562c567675
Samuel Baker <samuelcastro.baker@gmail.com>
2022-02-16 17:13:21 -0300

    init subscription usecase

---
+++ test/features/domain/usecase/subscription_notification_usecase_test.dart
@@ -0,0 +1,12 @@
+import 'package:agua_coach_app/features/domain/usecase/subscribe_notification_usecase.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  late final SubscribeNotificationUsecase usecase;
+
+  setUp(() {
+    usecase = SubscribeNotificationUsecase();
+  });
+
+  test('should subscribe notification', () {});
+}
