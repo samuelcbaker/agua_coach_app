29f586f5942f68e61cb77bb43a18b8eda1d0f749
Samuel Baker <samuelcastro.baker@gmail.com>
2022-02-18 09:09:44 -0300

    fix run configuration

--- lib/main_stg.dart
+++ lib/main_stg.dart
@@ -1,12 +1,22 @@
 import 'package:agua_coach_app/app.dart';
 import 'package:agua_coach_app/di/app_component.dart';
 import 'package:agua_coach_app/env/staging_build_config.dart';
+import 'package:agua_coach_app/injection_container.dart';
 import 'package:flutter/material.dart';
+import 'package:get_it/get_it.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  final sl = GetIt.instance;
+
   final buildConfig = StagingBuildConfig();
   await AppInjectionComponent.instance.registerModules(buildConfig);
+
+  final injectors = [
+    AppInjectionContainer(),
+  ];
+
+  await Future.wait(injectors.map((injector) async => {await injector.init(sl: sl)}));
 
   runApp(const WaterApp());
 }
