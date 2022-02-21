23eecc4f46f8a41e669aa757b9d09ec03713ebb2
Samuel Baker <samuelcastro.baker@gmail.com>
2022-02-21 17:30:39 -0300

    feat start schedule usecase

--- lib/core/usecase/errors/failures.dart
+++ lib/core/usecase/errors/failures.dart
@@ -11,3 +11,8 @@ class GetOnDbFailure extends Equatable implements Failure {
   @override
   List<Object?> get props => [];
 }
+
+class GenerateIdFailure extends Equatable implements Failure {
+  @override
+  List<Object?> get props => [];
+}
--- lib/modules/domain/usecase/start_schedule_notification_usecase.dart
+++ lib/modules/domain/usecase/start_schedule_notification_usecase.dart
@@ -1,5 +1,8 @@
+import 'dart:math';
+
 import 'package:agua_coach_app/core/usecase/errors/failures.dart';
 import 'package:agua_coach_app/core/usecase/usecase.dart';
+import 'package:agua_coach_app/modules/domain/entities/notification_entity.dart';
 import 'package:agua_coach_app/modules/domain/repositories/schedule_notification_repository.dart';
 import 'package:dartz/dartz.dart';
 
@@ -9,8 +12,22 @@ class StartScheduleNotificationUsecase extends Usecase<int, StartScheduleNotific
   StartScheduleNotificationUsecase({required this.repository});
 
   @override
-  Future<Either<Failure, int>> call(StartScheduleNotificationParams params) {
-    throw UnimplementedError();
+  Future<Either<Failure, int>> call(StartScheduleNotificationParams params) async {
+    final int id;
+
+    try {
+      id = Random().nextInt(1000); //TODO: improve id generator to avoid equal id
+    } catch (e) {
+      return Left(GenerateIdFailure());
+    }
+
+    final notificationEntity = NotificationEntity(
+      id: id,
+      title: params.title,
+      body: params.body,
+      scheduledDateTime: params.scheduledDateTime,
+    );
+    return repository.startDailyScheduleNotification(notificationEntity: notificationEntity);
   }
 }
 
